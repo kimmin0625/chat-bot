ff19893648fcdfd83fa695ac03ccb54204c69e94
kimmin0625 <ssh11900@gmail.com>
2026-08-20 09:29:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import PyPDF2
 import docx
 import os
 import time
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
